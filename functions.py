f99ddc1f05707e781f570e9d7ebc2c56a1867396
nick17773 <n.a.russell@student.saintkentigern.com>
2020-06-08 00:42:36 +0000

    made everything simpler

--- functions.py
+++ functions.py
@@ -8,40 +8,118 @@ def validator(prompt):
             value = float(input(prompt))
             true = False
         except:
-            print("That is not a valid int please input an integer")
+            print("That is not a valid float please input a float")
     return value
 
+def historyAdding(ap, shape, num, history):
+    history_item = ap + " of " + shape + " = " + str(num)    
+    history.append(history_item)
+    return history
+# area functions
 
-def area_main(shape, x, y):
-
-    if shape == "square":
-        return x * y
-
-    elif shape == "triangle":
-        area = 0.5 * x
-        return area * y
-    elif shape == "rectangle":
-        return x * y
-    elif shape == "parallelogram":
-        return x * y
-
-
-def circle_area(r):
+def circle_area(r, y):
     return math.pi * r * r
 
+def square_area(x, y):
+  return x * x
 
-def circle_perimeter(r):
+def rectangle_area(x,y):
+  return x * y
+
+def triangle_area(b,h):
+  return (0.5 * b) * h
+
+# perimeter functions
+
+def square_perimeter(x, y, z):
+  return 4 * x
+
+def rectangle_perimeter(x,y, z):
+  return (2 * x) + (2 * y)
+
+def circle_perimeter(r, x, y):
     return 2 * math.pi * r
 
+def triangle_perimeter(s1,s2,s3):
+  return s1 + s2 + s3
 
-def perimeter_3_sided(x, y, z):
-    return x + y + z
+def area_tool(oneInput, twoInput):
+  functions = {
+    "square" :square_area,
+    "rectangle" : rectangle_area,
+    "triangle" : triangle_area,
+    "circle" : circle_area
+  }
+  val = True
+  while val: 
+    shape = input("What shape do you want to find ")
+    if shape in oneInput:
+      x = validator("What is the radius or side-length ")
+      y = None
+    elif shape in twoInput:
+      x = validator("What is a side-length ")
+      y = validator("What is the other side-length ")
+    elif shape == "end":
+      break
+    else:
+      print("This is a shape that this tool is not designed to deal with")
+    function = functions.get(shape)
+    area = function(x, y)
+    area = [shape, area]
+    #print(history)
+    return area
+
+def perimeter_tool(oneInput, twoInputP, threeInput):
+  functions = {
+    "square" :square_perimeter,
+    "rectangle" : rectangle_perimeter,
+    "triangle" : triangle_perimeter,
+    "circle" : circle_perimeter
+  }
+  val = True
+  while val:
+    shape = input("What shape do you want to find ")
+    if shape in oneInput:
+      x = validator("what is the side length or radius ")
+      y = None
+      z = None
+    elif shape in twoInputP:
+      x = validator("What is the first side length")
+      y = validator("What is the second side length")
+      z = None
+    elif shape in threeInput:
+      x = validator("What is the first side length ")
+      y = validator("What is the second side length ")
+      z = validator("What is the third side lenght ")
+    function = functions.get(shape)
+    perimeter = function(x, y, z)
+    perimeter = [shape, perimeter]
+    #print(history)
+    return perimeter
 
 
-def perimeter_4_sided(shape, x, y):
-    if shape == "square":
-        return 4 * x
-    elif shape == "rectangle":
-        return (2 * x) + (2 * y)
-    elif shape == "parallelogram":
-        return (2 * x) + (2 * y)
+def main(history):
+  
+  oneInput = ["square", "circle"]
+  twoInput = ["triangle", "rectangle"]
+  twoInputP = ["rectangle"]
+  threeInput = ["triangle"]
+  area_perimeter = ["area", "perimeter"]
+  ap = input("Do you want to find area or perimeter ")
+  true = True
+  while true:
+
+    if ap in area_perimeter:
+      if ap == "perimeter":
+        num = perimeter_tool(oneInput, twoInputP, threeInput)
+        shape = num[0]
+        num1 = num[1]
+      elif ap == "area":
+        num = area_tool(oneInput, twoInput)
+        shape = num[0]
+        num1 = num[1]
+      #elif ap == "history":
+    else:
+      break
+    history = historyAdding(ap, shape, num1, history)
+    return history
--- main.py
+++ main.py
@@ -1,56 +1,10 @@
-from functions import validator, area_main, circle_area, perimeter_4_sided, circle_perimeter, perimeter_3_sided
+from functions import validator, area_tool, perimeter_tool, main
 
 true = True
-fourSided = ["square", "rectangle", "parallelogram"]
-threeSided = ["triangle"]
-circle = ["circle"]
+
 history = []
 area_perimeter = ["area", "perimeter"]
-while true:
-    ap = input("Do you want to find area or perimeter, or access history ")
-    if ap in area_perimeter:
-        if ap == "area":
-            shape = input("What shape ")
 
-            if shape == "circle":
-                r = validator("What is the radius of the circle ")
-                area = circle_area(r)
-                print(area)
 
-            else:
-                x = validator("What is the width of the shape ")
-                y = validator("What is the height of the shape ")
-                area = area_main(shape, x, y)
-                print(area)
-
-            history_item = ap + " of " + shape + " = " + str(area)    
-            history.append(history_item)
-        elif ap == "perimeter":
-            shape = input("What shape ")
-            if shape in fourSided:
-                if shape == "square":
-                    x = validator("x ")
-                    y = None
-                    perimeter = perimeter_4_sided("square", x, y)
-                    print(perimeter)
-                perimeter = perimeter_4_sided(shape, x, y)
-                print(perimeter)
-            elif shape in threeSided:
-                side1 = validator("side length 1 ")
-                side2 = validator("side length 2 ")
-                side3 = validator("side length 3 ")
-                perimeter = perimeter_3_sided(side1, side2, side3)
-                print(perimeter)
-            elif shape in circle:
-                r = validator("Radius ")
-                perimeter = circle_perimeter(r)
-                print(perimeter)
-            history_item = ap + " of " + shape + " = " + str(perimeter)
-            history.append(history_item)
-    elif ap == "history":
-        for i in history:
-          print(i)
-
-    else:
-        print("no. goodbye!")
-        true = False
+history = main(history)
+print(history[-1])
